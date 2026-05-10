52a25208a1040423346e55c4d42fc414b0d3d07e
Akshat Batra <batra.akshat25@gmail.com>
2026-05-10 19:01:32 +0530

    Update FRONTEND_URL to bitetrack-frontenddd.vercel.app

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(cors({
   origin: [
-    "https://bitetrack-frontendd.vercel.app",
+    "https://bitetrack-frontenddd.vercel.app",
     "https://bitetrack-frontendd-git-main-batraakshat25-5082s-projects.vercel.app",
     "https://bitetrack-frontendd-nk3t58d9f-batraakshat25-5082s-projects.vercel.app",
     "http://localhost:5173",
@@ -25,7 +25,7 @@ app.use(cors({
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true
-}));
+});
 
 const passport = require("passport");
 app.use(passport.initialize());
